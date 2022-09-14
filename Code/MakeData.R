4c822001596ffd5fa34d7936316aa0d785ec4c6f
Martyn Egan <87206678+MartynEgan@users.noreply.github.com>
2022-09-14 15:21:43 +0100

    Add observed values

--- Code/MakeData.R
+++ Code/MakeData.R
@@ -9,10 +9,13 @@
 # this script
 
 ## Create a vector for height
-height <- c()#rnorm(27, mean = 160, sd = 15)
+height <- c(182, 171, 183, 193, 175, 173, 173, 169, 184, 179, 160,
+            168, 182, 182, 188, 172, 165, 165, 171, 173, 180, 174,
+            154, 167, 180, 173, 182, 161, 165, 167, 157, 157, 185)#rnorm(27, mean = 160, sd = 15)
 
 ## Create a vector for sex
-sex <- c()#sample(c(0,1), replace=TRUE, size=27)
+sex <- c(0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 
+         0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0)#sample(c(0,1), replace=TRUE, size=27)
 
 ## Create a data.frame object from these vectors using cbind()
 data <- data.frame(cbind(height, sex))
